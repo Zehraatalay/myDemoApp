2fd04254ef8cfe710d3eafa08007d6a8efc5e47b
Zehraatalay <zatalayy168@gmail.com>
2024-03-12 20:51:48 +0300

    recent changes

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -13,26 +13,26 @@ import junit.framework.TestSuite;
 public class AppTest {
 
     public void testNull() {
-        assertFalse(App.calculatePrice(null, null, "message", 0));
+        assertFalse(new App().calculatePrice(null, null, "message", 0));
     }
     
     public void testFound() {
         ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
         ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        assertTrue(App.calculatePrice(array1, array2, "message is valid", 0));
+        assertTrue(new App().calculatePrice(array1, array2, "message is valid", 0));
     }
 
     
     public void testNotFound() {
         ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
         ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        assertFalse(App.calculatePrice(array1, array2, "message is not valid", 0));
+        assertFalse(new App().calculatePrice(array1, array2, "message is not valid", 0));
     }
     
 
     public void testEmptyArray() {
         ArrayList<Integer> array1 = new ArrayList<>();
         ArrayList<Integer> array2 = new ArrayList<>();
-        assertFalse(App.calculatePrice(array1, array2, "message", 0 ));
+        assertFalse(new App().calculatePrice(array1, array2, "message", 0 ));
     }
 }
