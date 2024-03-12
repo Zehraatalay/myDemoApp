b4a1478b39dedfc0f843b025f312452841906be0
Zehraatalay <zatalayy168@gmail.com>
2024-03-12 21:06:25 +0300

    test dosyasi duzenlemesi

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,5 +1,4 @@
 package com.mycompany.app;
-import java.lang.reflect.Array;
 import java.util.*;
 
 import junit.framework.Test;
@@ -9,31 +8,59 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-
-
-public class AppTest {
-
-    public void testNull() {
-        assertFalse(new App().calculatePrice(null, null, "message", 0));
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
     }
-    
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
     public void testFound() {
         ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
         ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        assertTrue(new App().calculatePrice(array1, array2, "message is valid", 0));
+        assertTrue(new App().calculatePrice(array1, array2, "test is valid", 0));
+
     }
 
-    
     public void testNotFound() {
         ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
         ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        assertFalse(new App().calculatePrice(array1, array2, "message is not valid", 0));
+        assertFalse(new App().calculatePrice(array1, array2, "test is not valid", 0));
+
     }
-    
 
     public void testEmptyArray() {
         ArrayList<Integer> array1 = new ArrayList<>();
         ArrayList<Integer> array2 = new ArrayList<>();
-        assertFalse(new App().calculatePrice(array1, array2, "message", 0 ));
+        assertFalse(new App().calculatePrice(array1, array2, "message", 0 ));
+
     }
+
+    public void testNull() {
+        assertFalse(new App().calculatePrice(null, null, "message", 0));
+
+    }
+
 }
